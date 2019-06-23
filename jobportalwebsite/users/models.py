7bbd232660d36578558d834454f5ae91576e2cce
Saloni Tyagi <saloni19tyagi@gmail.com>
2019-06-23 22:46:43 +0530

    CSS File added

--- jobportalwebsite/users/models.py
+++ jobportalwebsite/users/models.py
@@ -1,13 +1,10 @@
 from django.db import models
-from django.utils import timezone
 from django.contrib.auth.models import User
 
+class Profile(models.Model) :
+	user = models.OneToOneField(User , on_delete = models.CASCADE)
+	image = models.ImageField(default='default.jpg',upload_to = 'profile_pics')
+	bio = models.TextField(max_length=500, blank=True)
 
-class Post(models.Model) :
-	title = models.CharField(max_length = 100)
-	date_posted = models.DateTimeField(default = timezone.now)
-	content = models.TextField()
-	author = models.ForeignKey(User ,on_delete=models.CASCADE)
-
-	def __str__(self):
-		return self.title
+	def __str__(self) :
+		return f'{self.user.username} Profile'
