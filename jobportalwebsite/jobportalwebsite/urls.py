b8d09e7128815a4fea33fc8e6acf51b1a1182c45
Saloni Tyagi <saloni19tyagi@gmail.com>
2019-06-24 11:45:50 +0530

    changes

--- jobportalwebsite/jobportalwebsite/urls.py
+++ jobportalwebsite/jobportalwebsite/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from account import views
+# from account import views
 from django.urls import path,include
 from users import views as user_views
 from django.contrib.auth import views as auth_views
@@ -7,11 +7,11 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('',views.home,name='home'),
+    # path('',views.home,name='home'),
     # path('account/',include('account.urls')),
     path('opening/',include('jobopening.urls')),
 
-    path('account/',include('account.urls')),
+    # path('account/',include('account.urls')),
     path('register/',user_views.register,name='register') ,
     path('profile/',user_views.profile,name='profile') ,
 	path('login/',auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
--- jobportalwebsite/users/views.py
+++ jobportalwebsite/users/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render ,redirect
-#from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .form import UserRegisterForm
 from django.contrib.auth.decorators import login_required
