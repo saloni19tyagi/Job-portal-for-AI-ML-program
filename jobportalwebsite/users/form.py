8764c2e9460a32e46a23c1574ee82db78f263346
Saloni Tyagi <saloni19tyagi@gmail.com>
2019-06-24 22:13:41 +0530

    More Fields Added

--- jobportalwebsite/users/form.py
+++ jobportalwebsite/users/form.py
@@ -4,10 +4,20 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class UserRegisterForm(UserCreationForm) :
-	Email = forms.EmailField()
-	Contact_Number = forms.CharField()
-	Resume = forms.FileField()
+    first_name = forms.CharField()
+    middle_name = forms.CharField()
+    last_name = forms.CharField()
+    date_of_birth = forms.DateField()
+    email = forms.EmailField()
+    experience = forms.CharField()
+    password1 = None
+    password2 = None
+    Contact_Number = forms.CharField()
+    Resume = forms.FileField()
 
-	class meta :
-		model = User
-		fields = ['username' ,'Email', 'Contact Number' ,'Resume','password' ,'password1']
+    class meta :
+    	model = User
+    	fields = ['username' ,'email', 'Contact Number' ,'Resume']
+
+
+
