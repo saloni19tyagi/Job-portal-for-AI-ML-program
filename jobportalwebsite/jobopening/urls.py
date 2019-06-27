9a4ec87b9a0e4f8da4ca63d09d04ffcadae3533a
Yukta Anand <yukta.19feb@gmail.com>
2019-06-27 06:00:02 +0000

    Update urls.py

--- jobportalwebsite/jobopening/urls.py
+++ jobportalwebsite/jobopening/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('jobopening', views.jobopening, name='jobopening'),
     path('home', views.home, name='home'),
+    path('store_applications',views.store_applications,name='store_applications'),
 ]
