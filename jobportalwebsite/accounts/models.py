cbb3360aa043ed7098a2a2541223987076a2e2ea
Saloni Tyagi <saloni19tyagi@gmail.com>
2019-06-27 06:00:57 +0000

    Update models.py

--- jobportalwebsite/accounts/models.py
+++ jobportalwebsite/accounts/models.py
@@ -1,14 +1,24 @@
 from django.db import models
 from django.db.models import IntegerField
+from phonenumber_field.modelfields import PhoneNumberField
 
 class Register(models.Model) :
-	first_name = models.CharField(max_length = 50, default = "")
-	last_name = models.CharField(max_length = 50, default = "")
-	email = models.EmailField(max_length = 50)
-	mobile_number = models.IntegerField(default = "")
-	city = models.CharField(max_length = 50 , default = "")
-	tenth_marks = models.CharField(max_length = 50,default ="")
-	twelfth_marks = models.CharField(max_length = 50,default = "")
-	grad_year = models.IntegerField(default="")
 
-	# resume = models.FileField(default = "")
+
+	name = models.CharField(max_length = 50, default = "")
+	email = models.EmailField(max_length = 50, primary_key = True)
+	mobile_number = PhoneNumberField()
+	city = models.CharField(max_length = 50 , default = "")
+	tenth_marks = models.IntegerField(default ="")
+	twelfth_marks = models.IntegerField(default = "")
+	course = models.CharField(max_length = 50,default = "")
+	college_name = models.CharField(max_length = 100,default = "")
+	stream = models.CharField(max_length = 50,default = "")
+	graduation_year = models.IntegerField(default = "")
+	is_valid = models.BooleanField(default = False)
+	is_active = models.BooleanField(default = False)
+	password = models.CharField(default = "" ,max_length = 50)
+
+	def __str__(self):
+		return self.name
+
