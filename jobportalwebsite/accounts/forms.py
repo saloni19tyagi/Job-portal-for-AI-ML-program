ee600f9619be72a50d190fd47a3743ddcb093bd2
Saloni Tyagi <saloni19tyagi@gmail.com>
2019-06-27 06:00:31 +0000

    Update forms.py

--- jobportalwebsite/accounts/forms.py
+++ jobportalwebsite/accounts/forms.py
@@ -3,6 +3,15 @@ from .models import Register
 
 
 class UserRegisterForm(forms.ModelForm) :
+	COURSE = [
+	('btech','BTech'),
+	('be','BE'),
+	('bsc','BSC'),
+	('others','Ohers')
+	]
+	course = forms.CharField(label = "Select Your Course", widget = forms.Select(choices=COURSE))
+	# password = forms.CharField(widget=PasswordInput())
 	class Meta :
 		model = Register
-		fields = ['first_name' , 'last_name', 'email' , 'mobile_number' ,'city' ,'tenth_marks' , 'twelfth_marks' ,'grad_year']
+		fields = ['name', 'email' , 'mobile_number' ,'city' ,'tenth_marks' , 'twelfth_marks'  , 'course','college_name' ,'stream','graduation_year']
+
